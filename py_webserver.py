65d0febaf7b8659d2e28842fa2da1113ad36c7ec
Yongxin-Lu <simonlu@hotmail.com>
2019-06-24 14:21:29 +0800

    sdfsdf

--- py_webserver.py
+++ py_webserver.py
@@ -1,25 +1,17 @@
 import tornado.web
-import _thread
-import time
 
 from tornado.web import RequestHandler
+from tornado.websocket import WebSocketHandler
 from tornado.ioloop import IOLoop
 
-class IndexHandler(RequestHandler):
+class IndexHandler(RequestHandler):  #返回控制页面
     def get(self):
         self.render("ws.html")
 
-def thread_sayhi(threadName):
-    while True:
-        print("hi,simon")
-        time.sleep(1)
-        print("nice to meet u")
-        time.sleep(1)
-
 if __name__=="__main__":
-    _thread.start_new_thread( thread_sayhi, ("Thread-1",) )   
+    
     app=tornado.web.Application([
-        (r'/', IndexHandler),
+        (r'/', IndexHandler)
     ])
     app.listen(8081)
     IOLoop.current().start()
